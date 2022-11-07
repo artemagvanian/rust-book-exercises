ba8fa6eb784c71f0c29773fae212bed3f105f2ff
Artem Agvanian <artemagvanian@icloud.com>
2022-11-07 13:07:12 -0500

    finish week4 exercises

--- week4/starter/src/p1_iter.rs
+++ week4/starter/src/p1_iter.rs
@@ -31,6 +31,100 @@
 
 
 // Your implementation goes here!
+struct CacheableIterator<T, A>
+    where T: Iterator<Item=A>, A: Clone {
+    iter: T,
+    cache: Vec<A>,
+    last_idx: Option<usize>
+}
+
+impl<T, A> CacheableIterator<T, A>
+    where T: Iterator<Item=A>, A: Clone {
+    fn new(iter: T) -> CacheableIterator<T, A> {
+        CacheableIterator { iter, cache: vec![], last_idx: None }
+    }
+
+    fn next(&mut self) -> Option<A> {
+        let next_elt = self.iter.next();
+        match next_elt {
+            None => None,
+            Some(next_val) => {
+                self.cache.push(next_val.clone());
+                match self.last_idx {
+                    None => { self.last_idx = Some(0) }
+                    Some(prev_last_idx) => { self.last_idx = Some(prev_last_idx + 1) }
+                }
+                return Some(next_val);
+            }
+        }
+    }
+
+    fn ith(&mut self, i: usize) -> Option<A> {
+        while self.last_idx.and_then(|idx| Some(idx < i)).unwrap_or(true) {
+            match self.next() {
+                None => { return None; },
+                Some(_) => ()
+            }
+        }
+        return Some(self.cache[i].clone());
+    }
+}
+
+struct CartesianIterator<T, U, A, B>
+    where T: Iterator<Item=A>, U: Iterator<Item=B>, A: Clone, B: Clone {
+    first: CacheableIterator<T, A>,
+    second: CacheableIterator<U, B>,
+    idx_first: usize,
+    idx_second: usize,
+}
+
+impl<T, U, A, B> CartesianIterator<T, U, A, B>
+    where T: Iterator<Item=A>, U: Iterator<Item=B>, A: Clone, B: Clone {
+    fn new(first: T, second: U) -> CartesianIterator<T, U, A, B> {
+        CartesianIterator { first: CacheableIterator::new(first),
+            second: CacheableIterator::new(second), idx_first: 0, idx_second: 0}
+    }
+}
+
+impl<T, U, A, B> Iterator for CartesianIterator<T, U, A, B>
+    where T: Iterator<Item=A>, U: Iterator<Item=B>, A: Clone, B: Clone {
+    type Item = (A, B);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let nxt_a = self.first.ith(self.idx_first);
+            let nxt_b = self.second.ith(self.idx_second);
+            match nxt_a {
+                None => { return None; },
+                Some(a) => {
+                    match nxt_b {
+                        None => {
+                            self.idx_first += 1;
+                            self.idx_second = 0;
+                            continue;
+                        }
+                        Some(b) => {
+                            self.idx_second += 1;
+                            return Some((a, b));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+trait Cartesian<T, U, A, B>
+    where T: Iterator<Item=A>, U: Iterator<Item=B>, A: Clone, B: Clone {
+    fn cartesian_product(self, other: U) -> CartesianIterator<T, U, A, B>;
+}
+
+impl<T, U, A, B> Cartesian<T, U, A, B> for T
+    where T: Iterator<Item=A>, U: Iterator<Item=B>, A: Clone, B: Clone {
+    fn cartesian_product(self, other: U) -> CartesianIterator<T, U, A, B> {
+        CartesianIterator::new(self, other)
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -46,6 +140,14 @@ mod test {
         assert_eq!(
             product.collect::<HashSet<_>>(),
             hashset![(1, 3), (1, 4), (2, 3), (2, 4)]
-        )
+        );
+
+        let v1 = vec![1, 2, 3];
+        let v2 = vec![4, 5];
+        let product = v1.into_iter().cartesian_product(v2.into_iter());
+        assert_eq!(
+            product.collect::<Vec<_>>(),
+            vec![(1, 4), (1, 5), (2, 4), (2, 5), (3, 4), (3,5)]
+        );
     }
 }
--- week4/starter/src/p2_tui.rs
+++ week4/starter/src/p2_tui.rs
@@ -16,12 +16,99 @@
 //! contain data structures that represent Text, Heading, and Container. You should
 //! replicate the behavior of `main` in `tui.cpp` into the `container_test` function.
 
+use std::cmp::max;
+
+struct Dimensions {
+    width: usize,
+    height: usize
+}
+
+struct Text {
+    text: String,
+}
+
+trait Element {
+    fn dimensions(&self) -> Dimensions;
+    fn render(&self);
+}
+
+impl Element for Text {
+    fn dimensions(&self) -> Dimensions {
+        Dimensions { width: self.text.len(), height: 1 }
+    }
+
+    fn render(&self) {
+        print!("{}", self.text);
+    }
+}
+
+struct Heading {
+    inner_text: Text
+}
+
+impl Element for Heading {
+    fn dimensions(&self) -> Dimensions {
+        self.inner_text.dimensions()
+    }
+
+    fn render(&self) {
+        print!("{}", self.inner_text.text);
+    }
+}
+
+struct Container {
+    children: Vec<Box<dyn Element>>,
+}
+
+impl Container {
+    fn dimensions(&self) -> Dimensions {
+        let mut max_width: usize = 0;
+        let mut sum_height: usize = 0;
+        for child in &self.children {
+            let dims: Dimensions = child.dimensions();
+            max_width = max(max_width, dims.width);
+            sum_height += dims.height;
+        }
+        return Dimensions{ width: max_width + 2, height: sum_height};
+    }
+
+    fn render(&self) {
+        let dims = self.dimensions();
+        let render_line = || {
+            print!("+");
+            for _ in 0..dims.width - 2 {
+                print!("-");
+            }
+            println!("+");
+        };
+        render_line();
+
+        for child in &self.children {
+            let child_dims = child.dimensions();
+            print!("|");
+            child.render();
+            for _ in 0..dims.width - 2 - child_dims.width {
+                print!(" ");
+            }
+            println!("|");
+        }
+        render_line();
+    }
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
     fn container_test() {
-        // Your unit test goes here!
+        let text = Heading { inner_text: Text { text: String::from("Hello world") }};
+        let text2 = Text { text: String::from("This is a long string of text") };
+        let mut children: Vec<Box<dyn Element>> = vec![];
+
+        children.push(Box::new(text));
+        children.push(Box::new(text2));
+        let container = Container { children };
+
+        container.render();
     }
 }
